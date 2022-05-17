ef64855b7036be96aa2f11cbbf852d289192a5bc
Aziz <azizulin1@gmail.com>
2022-05-16 18:33:44 -0700

    Add additional assets

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthUI : MonoBehaviour
+public class UI : MonoBehaviour
 {
     GameObject player;
     Text healthText;
+    Text timerText;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player");
-        healthText = this.GetComponent<Text>();
+        healthText = this.gameObject.transform.GetChild(0).gameObject.GetComponent<Text>();
+        healthText = this.gameObject.transform.GetChild(1).gameObject.GetComponent<Text>();
     }
 
     // Update is called once per frame
