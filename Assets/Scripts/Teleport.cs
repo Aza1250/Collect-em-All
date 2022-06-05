6052823a842d52e2f6bc40deb84d2bc4d3f4a465
Aziz <azizulin1@gmail.com>
2022-06-05 10:05:38 -0700

    fix number of coins picked up

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -6,11 +6,11 @@ using UnityEngine.SceneManagement;
 public class Teleport : MonoBehaviour
 {
     public void Start() {
-        print(GameObject.FindObjectsOfType<Coin>().Length);
+        //print(GameObject.FindObjectsOfType<Coin>().Length);
     }
 
     void OnTriggerEnter2D(Collider2D other) {
-        print(GameObject.FindObjectsOfType<Coin>().Length);
+        //print(GameObject.FindObjectsOfType<Coin>().Length);
 
         if (other.gameObject.tag == "Player" && GameObject.FindObjectsOfType<Coin>().Length == 0) {
             SceneManager.LoadScene("WinScreen");
--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -8,11 +8,14 @@ public class UI : MonoBehaviour
     GameObject player;
     Text healthText;
     public Text timerText;
+    public Text coinsCollectedText;
     public float time;
+    int totalCoins;
 
     void Awake() {
         // Don't destroy the timer text
         DontDestroyOnLoad(transform.gameObject);
+        totalCoins = GameObject.FindObjectsOfType<Coin>().Length;
     }
 
     // Start is called before the first frame update
@@ -20,12 +23,15 @@ public class UI : MonoBehaviour
     {
         player = GameObject.FindWithTag("Player");
         healthText = this.gameObject.transform.GetChild(0).gameObject.GetComponent<Text>();
+        coinsCollectedText = this.gameObject.transform.GetChild(2).gameObject.GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
         healthText.text = "Health: " + player.GetComponent<Player>().health.ToString();
+        print(totalCoins + "  " + GameObject.FindObjectsOfType<Coin>().Length);
+        coinsCollectedText.text = "Coins: " +  (totalCoins - GameObject.FindObjectsOfType<Coin>().Length);
         if (Input.GetKeyDown(KeyCode.RightShift)) time += Time.deltaTime * 2;
         else time += Time.deltaTime;
         timerText.text = "Time: " + time.ToString("0.00");
